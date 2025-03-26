f0fd5dad493199942dd58a36598c111d1a411d85
WillCody <45133150+WillCody@users.noreply.github.com>
2025-03-26 01:24:39 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,30 @@
 const cacheName = 'movement-picker-v2';
-const filesToCache = [
-  '/movement-picker/',
-  '/movement-picker/index.html',
-  '/movement-picker/manifest.json',
-  '/movement-picker/icon-192.png'
-];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // <- Force immediate activation
+  event.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
+      return cache.addAll([
+        '/movement-picker/',
+        '/movement-picker/index.html',
+        '/movement-picker/manifest.json',
+        '/movement-picker/icon-192.png'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('activate', (event) => {
+  clients.claim(); // <- Take over any open tabs
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
